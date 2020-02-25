0e5b66d89a50bdd869c58c5ffc2464ca55c38705
Guilherme <guilherme.gl1997@gmail.com>
2020-02-25 15:29:26 -0300

    botoes programados e personalizados

--- public/index.js
+++ public/index.js
@@ -1,6 +1,20 @@
+let inputFile = document.querySelector('#btnFile');
+
+function getFile() {
+    inputFile.click();
+}
+
+function showFile() {
+    let list = document.querySelector('.fileList');
+    list.innerHTML = '<p>File: ' + event.target.files[0].name + '</p>';
+    list.style.color = '#00e600';
+
+    let teste = document.querySelector('.btn');
+    teste.style.display = 'inline-block';
+}
+
 function data() {
     let footer = document.querySelector('#footer');
     let year = new Date().getFullYear();
-
     footer.innerHTML = '<p>Copyright &copy ' + year + ' - Cryptography | Todos os direitos reservados.</p>';
 }
