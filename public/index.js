fedb14159e4f79df123c84acdfb52371250dd0e6
Guilherme <guilherme.gl1997@gmail.com>
2020-02-29 19:35:17 -0300

    arquivo no console

--- public/index.js
+++ public/index.js
@@ -4,13 +4,17 @@ function getFile() {
     inputFile.click();
 }
 
-function showFile() {
+function showFile(arquivo) {
     let list = document.querySelector('.fileList');
+
     list.innerHTML = '<p>File: ' + event.target.files[0].name + '</p>';
     list.style.color = '#00e600';
 
-    let teste = document.querySelector('.btn');
-    teste.style.display = 'inline-block';
+    let buttonsCrypt = document.querySelector('.btn');
+    buttonsCrypt.style.display = 'inline-block';
+
+    let file = arquivo.files[0];
+    console.log(file);
 }
 
 function data() {
