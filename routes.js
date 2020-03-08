02fa35989d4282b59a3250ad0a971a4fdebd12af
paulodelia <paulohdelia@gmail.com>
2020-03-08 16:54:16 -0300

    download encrypted pdf

--- routes.js
+++ routes.js
@@ -16,43 +16,50 @@ const storage = multer.diskStorage({
         cb(null, './uploads')
     },
     filename: function (req, file, cb) {
-        cb(null, 'crypt-' + file.originalname);
+        // alterei para esse nome padrão pra n lotar a pasta do servidor com vários arquivos novos
+        cb(null, 'crypt-this.pdf');
     }
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage });
 
 // rota padrão para quando acessa o localhost:5000 
 routes.get('/', function (req, res) {
     return res.render("index")
 });
 
-// rota para o post do localhost:5000
-routes.post('/', upload.single('fileupload'), function (req, res, next) {
-    let { filename } = req.file;
+// rota para donwload do arquivo
+routes.get('/download', function (req, res) {
+    var filePath = "./uploads/encrypted-file.pdf"; //caminho do arquivo completo
+    var fileName = "encrypted-file.pdf"; // O nome padrão que o browser vai usar pra fazer download
 
-    let dataBuffer = fs.readFileSync("./uploads/" + filename);
+    return res.download(filePath, fileName)
+});
+
+// rota para o post do localhost:5000
+routes.post('/', upload.single('fileupload'), function (req, res) {
+
+    // forcei o caminho pra ir direto no mesmo nome que forcei no storage
+    let dataBuffer = fs.readFileSync("./uploads/crypt-this.pdf");
 
     // função assíncrona para o valor ser retornado somente quando o texto do pdf for extraído e movido para a const text
-    async function read_pdf() {
+    async function createPDF() {
         const text = await pdf(dataBuffer).then(function (data) {
             return (data.text);
         })
 
         const cryptr = new Cryptr('senha_secreta');
         const encryptedString = cryptr.encrypt(text)
-        const decryptedString = cryptr.decrypt(encryptedString);
 
-        myDoc.pipe(fs.createWriteStream('public/uploads/encrypted-file.pdf'));
-
+        // coloquei pra ele criar o arquivo no back mesmo
+        myDoc.pipe(fs.createWriteStream('./uploads/encrypted-file.pdf'));
         myDoc.text(encryptedString,100,100);
 
-        myDoc.end();
-
-        return res.send(`PDF Criptografado:\n\n${encryptedString}\nPDF Original:\n\n${decryptedString}`);
+        // redireciona para a rota responsável pelo download
+        return res.redirect('download');
     }
     
-    read_pdf();
+    createPDF();
 });
 
 module.exports = routes;
